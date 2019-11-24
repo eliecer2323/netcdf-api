4ec698c8e67d55d84d1562b0b09f6182fc8de4bf
Jorge Eliecer Sierra Torres <jorge.sierra@uproar.car>
2019-11-24 17:42:29 -0500

    Init

---
+++ src/main/java/co/edu/udistrital/informatica/netcdfapi/NetcdfApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.informatica.netcdfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcdfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetcdfApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udistrital/informatica/netcdfapi/NetcdfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udistrital.informatica.netcdfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetcdfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
