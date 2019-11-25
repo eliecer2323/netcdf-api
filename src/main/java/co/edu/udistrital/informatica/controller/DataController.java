11fbc2a9107b371609aa455ea6d1f2e6938b2766
Jorge Eliecer Sierra Torres <jorge.sierra@uproar.car>
2019-11-24 20:42:59 -0500

    Fixed CORS

--- src/main/java/co/edu/udistrital/informatica/controller/DataController.java
+++ src/main/java/co/edu/udistrital/informatica/controller/DataController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,13 @@ public class DataController {
 	@Autowired
 	private IFileReader fileReader;
 
+	@CrossOrigin
 	@GetMapping("/all")
 	public List<String[]> getAll() throws IOException {
 		return fileReader.readNetCDF();
 	}
 
+	@CrossOrigin
 	@GetMapping("/variable/{name}")
 	public List getVariable(@PathVariable(value = "name") String varName) throws IOException {
 		return fileReader.ReadVariable(varName);
